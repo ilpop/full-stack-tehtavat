10d0c1cb19b6b3dd103b8cdb7595fe3618844033
Ilkka Ihalainen <50838855+ilpop@users.noreply.github.com>
2019-11-04 17:55:28 +0200

    Add files via upload

---
+++ osa2/2.1-2.5Kurssitiedot/src/components/Course.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Course = ({ course }) => {
+    return (
+      
+      <div> 
+      <Header name = {course.name} />
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
+      </div>
+    )
+  }
+  
+  const Header = ({ name }) => (
+    
+    <h1>{name}</h1>
+
+    )
+  
+  const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return (
+      <div>
+      <p> <strong> Tehtäviä yhteensä: </strong> <em> {total} </em> </p>
+      </div>
+    )
+  }
+  
+  const Part = ({name, exercises}) => (
+    <div>
+    <p>
+    
+    {name} {exercises}
+        
+    </p>
+    </div>
+  )
+
+  const Content = ({ parts }) => (
+    <div>
+      {parts.map(part => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+      </div>
+  )
+
+  export default Course;
